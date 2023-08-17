30c87f9a5d4556aee55ecb9538db4e1e81d25a70
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-17 16:47:11 -0400

    complete and bonus

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+class Stack
+  def initialize(limit = Float::INFINITY)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(val)
+    raise 'Stack Overflow' if at_limit?
+
+    @stack.push val
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.size
+  end
+
+  def empty?
+    @stack.size == 0
+  end
+
+  def full?
+    at_limit?
+  end
+
+  def search(val)
+    @stack.index(val) ? (@stack.size - 1) - @stack.index(val) : -1
+  end
+
+  private
+
+  def at_limit?
+    @stack.size == @limit
+  end
+end
